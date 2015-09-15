61d62fc6b6576cb71b8731adc79b733bd7f4585f
ocdocdetc <ocdocdetc@gmail.com>
2015-09-15 15:55:18 -0500

    moved blog templates to /blog/templates/blog/

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'tinymce',
+    'blog',
 )
 
 MIDDLEWARE_CLASSES = (
