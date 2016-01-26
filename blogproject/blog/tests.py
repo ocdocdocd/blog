165ac1f3e4a0160d1c6d222a092e3372c57438a6
ocdocdetc <ocdocdetc@gmail.com>
2016-01-26 16:23:42 -0600

    Added test for category view

--- blogproject/blog/tests.py
+++ blogproject/blog/tests.py
@@ -24,9 +24,16 @@ class BlogPostFactory(factory.DjangoModelFactory):
     pubDate = factory.fuzzy.FuzzyDate(datetime.date(2013, 1, 1))
     body = random_string(random.randint(400, 2500))
 
+    @factory.post_generation
+    def cats(self, create, extracted, **kwargs):
+        if extracted and type(extracted) == list:
+            for cat in extracted:
+                self.categories.add(cat)
+        else:
+            self.categories.add('a', 'b', 'c')
+
 
 class TestBlogViews(TestCase):
-
 
     def test_index_with_no_posts(self):
         '''
@@ -99,7 +106,7 @@ class TestBlogViews(TestCase):
         '''
         BlogPostFactory.create(title='test post')
         # Test getting a post that exists
-        resp = self.client.get(reverse('post', 
+        resp = self.client.get(reverse('post',
                                        kwargs={'blog_post_slug': 'test-post'}))
         # Title of post should be rendered twice in page
         self.assertContains(resp, 'test post', 2)
@@ -134,3 +141,32 @@ class TestBlogViews(TestCase):
         resp = self.client.get(reverse('archive',
                                        kwargs={'archive_slug': 'July'}))
         self.assertContains(resp, "There was an error", 1)
+
+    def test_category(self):
+        '''
+        Tests the category view.
+        '''
+        BlogPostFactory.create(title='post1', cats=['test'])
+        BlogPostFactory.create(title='post2', cats=['test', 'test2'])
+        BlogPostFactory.create(title='post3', cats=[])
+        BlogPostFactory.create_batch(8)
+
+        # test on an input that should return 2 results, post1 and post2
+        resp = self.client.get(reverse('category',
+                                       kwargs={'cat_slug': 'test'}))
+        self.assertEqual(len(resp.context['posts']), 2)
+        # the page will contain 2 hits for each title due to the URL slug
+        self.assertContains(resp, 'post1', 2)
+        self.assertContains(resp, 'post2', 2)
+
+        # test on an input that should return no results
+        resp = self.client.get(reverse('category',
+                                       kwargs={'cat_slug': 'foo'}))
+        self.assertEqual(len(resp.context['posts']), 0)
+        self.assertContains(resp, "There aren't any blog posts yet!", 1)
+
+        # test on an input that will become blank after processing
+        resp = self.client.get(reverse('category',
+                                       kwargs={'cat_slug': '-'}))
+        self.assertEqual(len(resp.context['posts']), 0)
+        self.assertContains(resp, "There aren't any blog posts yet!", 1)
